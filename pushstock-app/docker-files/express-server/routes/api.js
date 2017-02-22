2e3c8d68fbfc584e36d6a6400b48956455918c7c
Andres Sapper <sapper@SPL08.local>
2017-02-21 19:11:36 -0600

    fix file structure for Express API files

--- pushstock-app/docker-files/express-server/routes/api.js
+++ pushstock-app/docker-files/express-server/routes/api.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-// Get API listing
-router.get('/', (req, res) => {
-    res.send('api works!');
+
+// Route always called (verifications done here)
+router.use(function(req, res, next) {
+    // logging
+    console.log('Some validation (should be) happening here!');
+    next(); // next route
+});
+
+// Main route (ex: /api/)
+router.get('/', function(req, res) {
+    console.log("GET: /api/ being accessed")
+    res.json({ message: 'Hooray! Welcome to our API!' });
+});
+
+// Single click POST route (ex: /api/singleClick)
+router.post('/singleClick', function(req, res) {
+    button_id = req.body.buttonid
+    click_timestamp = req.body.click_timestamp
+    console.log("POST: Single Click by button " +
+        button_id + " at " + click_timestamp);
+    res.json({ message: 'Single click acknowledged' });
 });
 
 module.exports = router;
--- pushstock-app/docker-files/express-server/server.js
+++ pushstock-app/docker-files/express-server/server.js
@@ -1,24 +1,27 @@
 // Get dependencies
 const express = require('express');
-const path = require('path');
+const app = express();
+//const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // Get our API routes
-const api = require('./routes/api');
-
-const app = express();
+const api_routes = require('./routes/api');
 
 // Parsers for POST data
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// Set our api routes
-app.use('/', api);
-
-// Get port from environment and store in Express.
+// Get port from environment and store in Express
 const port = process.env.PORT || '3000';
 app.set('port', port);
+
+// Connect to DB
+//mongoose.connect('mongodb://127.0.0.1/testdb');
+
+// Set our api routes
+app.use('/api', api_routes);
 
 // Create HTTP Server
 const server = http.createServer(app);
