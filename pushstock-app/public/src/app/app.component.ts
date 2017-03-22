54615507a70d354d9d53d6e4325ab45bacda5eed
James Saul <saul@SPL09.local>
2017-03-22 01:57:53 -0500

    self serving button components

--- pushstock-app/public/src/app/app.component.ts
+++ pushstock-app/public/src/app/app.component.ts
@@ -9,6 +9,7 @@
 * ======			========		===========
 * Saul			  03/16/17		getEmployees added
 * Saul        03/20/17    implemented Delete and Add employees functionality
+* Saul        03/21/17    Moved functionality for buttons into button components
 */
 
 import { Component } from '@angular/core';
@@ -35,9 +36,6 @@ export class AppComponent {
     lastName: String = "Saul";
     role: String = "Manager";
 
-    // Empty list of buttons to be populated by DB
-    buttons: any[] = [];
-
     // Empty list of employess to be populated by DB
     employees: any[] = [];
 
@@ -45,9 +43,6 @@ export class AppComponent {
     welcomeView: boolean = true;
     buttonView: boolean = false;
     workerView: boolean = false;
-
-    // Boolean flag that controls if the buttonform is displayed
-    buttonActive: boolean = false;
 
     /*  Functions called to change the current view of the client app
     *   may be a good idea to try figure out to combine these. A fuction that
@@ -74,42 +69,12 @@ export class AppComponent {
     // Does anyone know what this does?
     constructor(private http: Http) {}
 
-    // Used to set buttonActive variable to true
-    setTrue() {
-      this.buttonActive = true;
-      console.log("set buttonActive to true");
-    }
-
     // Angular 2 Life Cycle event whem component has been initialized
     ngOnInit() {
-        this.getAllButtons();
         this.getAllEmployees();
         //this.deleteAllEmployees();
     }
-
-    // Function called when a button is created
-	  onButtonCreated(buttonId: number, clickTimestamp: Date, buttonDescription: string){
-      this.http.post(`${this.API}/addButton`, { buttonId, clickTimestamp, buttonDescription })
-     .map(res => res.json())
-     .subscribe(() => {
-       this.getAllButtons();
-     })
-
-     console.log("button added");
-	  }
-
-
-    // Function that returns all buttons from the API
-    getAllButtons() {
-        this.http.get(`${this.API}/buttons`)
-            .map(res => res.json())
-            .subscribe(buttons => {
-                console.log(buttons);
-                this.buttons = buttons;
-            })
-    }
-
-    // Function that returns all buttons from the API
+    // Function that returns all employees from the API
     getAllEmployees() {
         this.http.get(`${this.API}/employees`)
             .map(res => res.json())
