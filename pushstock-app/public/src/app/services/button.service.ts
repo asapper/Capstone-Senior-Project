774406eaca4d3053cbd2b2c1f09aeb833f1aafff
Collin Rapp <collin_rapp@baylor.edu>
2017-03-28 17:39:52 -0500

    Preemptively added service methods for button-detail component.

--- pushstock-app/public/src/app/services/button.service.ts
+++ pushstock-app/public/src/app/services/button.service.ts
@@ -19,61 +19,31 @@ import { ApiSettings } from './api-settings';
 
 @Injectable()
 export class ButtonService {
-	// API path
-	//api = ApiSettings.API;
-
-	//api: String = ApiSettings.API;
-
 	api: String;
-
 	constructor(private http: Http) {
-		//apiSuffix: String = 'buttons';
-		//fullApiPath: String = ApiSettings.API + '/buttons';
-
 		this.api = ApiSettings.API;
 	}
 
+	// Get the information for a specific button
+	getButton(macAddr: String) {
+		return this.http.get(`${this.api}/buttons/${macAddr}`)
+			.map(res => res.json());
+	}
+	
 	// Function that returns all buttons from the API
 	getAllButtons() {
-		/*
-		 * TODO change code from component
-		 */
-		/*
-		this.http.get(`${this.fullApiPath}`)
-			.map(res => res.json())
-			.subscribe(buttons => {
-				//console.log(buttons);
-				this.buttonsList = buttons;
-			})
-			*/
-
-		//return this.http.get(`${this.fullApiPath}`)
 		return this.http.get(`${this.api}/buttons`)
+			.map(res => res.json());
+	}
+
+	// Update the information for a button
+	updateButton(macAddr: String, description: String) {
+		return this.http.put(`${this.api}/buttons/${macAddr}`, { description })
 			.map(res => res.json());
 	}
 
 	// Deletes a button based on its mac address
 	deleteButton(macAddr: String) {
-		/*
-		 * TODO change code from component
-		 */
-
-		/*
-		this.http.delete(`${this.fullApiPath}/${macAddr}`)
-			.map(res => res.json())
-			.subscribe(buttons => {
-				console.log(buttons);
-				this.buttonList = buttons;
-			}
-
-			 // this shouldn't be done here; write a wrapper in the component that
-			 // calls deleteButton and then getAllButtons
-			console.log('deleting button: ' + macAddr);
-			this.getAllButtons;
-			*/
-
-		 // TODO get back error message from api and display to user
-		 //this.http.delete(`${this.fullApiPath}/${macAddr}`);
 		return this.http.delete(`${this.api}/buttons/${macAddr}`)
 			.map(res => res.json());
 	}
