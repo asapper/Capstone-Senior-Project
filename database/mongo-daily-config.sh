7c574ca2f611b687669e8438a5eb9e4018988caf
Andres Sapper <sapper@SPL08.local>
2017-04-03 17:25:48 -0500

    fix typo on script to delete buttons

--- database/mongo-daily-config.sh
+++ database/mongo-daily-config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Delete unused buttons
-docker exec 201701pvs02_database_1 mongo -u pushstock-api -p Capstone_PVS022017API --authenticationDatabase pushstock-app pushstock-app delete-unused-buttons.js
+docker exec 201701pvs02_database_1 mongo -u pushstock-api -p Capstone_PVS022017API --authenticationDatabase pushstock-app pushstock-app delete_unused_buttons.js
