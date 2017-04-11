c04d7749b61db5d581c18e99d3b7f560a067ee26
Collin Rapp <collin_rapp@baylor.edu>
2017-04-11 18:37:00 -0500

    Continued automating mobile app build process.

--- build_mobile_app.sh
+++ build_mobile_app.sh
@@ -3,8 +3,12 @@
 direxists=`ls ./pushstock-app | grep -c -E "^mobile-app$"`
 if [[ "$direxists" -eq "0" ]] then
     ionic start --v2 --ts --appname "PushStock" mobile-app
-    # Script is being run in bash shell so assume it's on macOS w/ XCode
-    ionic platform add ios android
 fi
+
+cd mobile-app
+
+# TODO check for ios and android first
+# Script is being run in bash shell so assume it's on macOS w/ XCode
+ionic platform add ios android
 
 #cp -R
