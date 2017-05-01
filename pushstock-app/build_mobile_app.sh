e0e4f0e6dc48f2df23159c529540fb9e54cd7f47
Collin Rapp <collin_rapp@baylor.edu>
2017-05-01 00:47:04 -0500

    Altered automated build of mobile app.

--- pushstock-app/build_mobile_app.sh
+++ pushstock-app/build_mobile_app.sh
@@ -5,9 +5,12 @@
 #cd ./pushstock-app
 
 # If mobile-app directory doesn't exist, make Ionic project
+# *should* never execute
 printf "Creating mobile app project...\n"
+newproj=false
 if [[ ! -d "mobile-app" ]]; then
     ionic start --v2 --ts --appname "PushStock" mobile-app blank
+    newproj=true
 fi
 
 # Ensure that all intended platforms added
@@ -20,21 +23,24 @@ if [[ ! -d "platforms/android" ]]; then
     ionic platform add android
 fi
 
-# Delete all old source files except theme/variables.scss
+# Delete all old source files except theme/variables.scss and index.html
 cd src
-rm -rf `ls | grep -v "theme"`
+if [[ "$newproj" = true ]]; then
+    exp="(theme)"
+else
+    exp="(them|index\.html)"
+fi
+rm -rf `ls | grep -v -E "$exp"`
 
 # Copy over all website source files
 printf "Copying over source files from web directory...\n"
-cp -R ../../public/src/ .
+cp -Rn ../../public/src/ .
 # Move main.ts to app/
 mv main.ts app/
 
 # Run python script to change source files
-printf "Modifying source for mobile...\n"
-../../make_mobile_files.py
+#printf "Modifying source for mobile...\n"
+#../../make_mobile_files.py
 
-
-
-#cd ?/mobile-app
-#npm install
+cd ..
+npm install
