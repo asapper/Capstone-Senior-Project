6ea334896688224a6d5b81f9128511e2c960c162
Collin Rapp <collin_rapp@baylor.edu>
2017-05-02 00:33:12 -0500

    Modified polyfills.ts to be mobile-compatible.

--- pushstock-app/public/src/polyfills.ts
+++ pushstock-app/public/src/polyfills.ts
@@ -1,6 +1,7 @@
 // This file includes polyfills needed by Angular and is loaded before the app.
 // You can add your own extra polyfills to this file.
 
+import 'reflect-metadata';
 import 'core-js/es7/reflect';
 import 'core-js/es6/reflect';
 
