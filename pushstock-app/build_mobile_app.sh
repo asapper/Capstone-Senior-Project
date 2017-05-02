95f1cd41800c8a41ad91500f7467cc44cb004ca8
Collin Rapp <collin_rapp@baylor.edu>
2017-05-01 23:38:40 -0500

    Finished writing script for automated mobile app build.

--- pushstock-app/build_mobile_app.sh
+++ pushstock-app/build_mobile_app.sh
@@ -2,25 +2,27 @@
 
 ### Script runs under the assumption of proper environment (i.e. has all dependencies) ###
 
-#cd ./pushstock-app
-
-# If mobile-app directory doesn't exist, make Ionic project
-# *should* never execute
-printf "Creating mobile app project...\n"
+# If mobile-app directory doesn't exist, make new Ionic project
+# This *should* never execute
 newproj=false
 if [[ ! -d "mobile-app" ]]; then
+    echo "Creating mobile app project..."
     ionic start --v2 --ts --appname "PushStock" mobile-app blank
     newproj=true
+    echo "Created project"
 fi
 
 # Ensure that all intended platforms added
-printf "Adding target platforms...\n"
 cd mobile-app
 if [[ ! -d "platforms/ios" ]]; then
+    echo "Adding ios platform..."
     ionic platform add ios
+    echo "Added ios platform"
 fi
 if [[ ! -d "platforms/android" ]]; then
+    echo "Adding android platform..."
     ionic platform add android
+    echo "Added android platform"
 fi
 
 # Delete all old source files except theme/variables.scss and index.html
@@ -28,19 +30,25 @@ cd src
 if [[ "$newproj" = true ]]; then
     exp="(theme)"
 else
-    exp="(them|index\.html)"
+    exp="(theme|index\.html|app)"
 fi
 rm -rf `ls | grep -v -E "$exp"`
 
 # Copy over all website source files
-printf "Copying over source files from web directory...\n"
+echo "Copying over source files from web directory..."
 cp -Rn ../../public/src/ .
+
 # Move main.ts to app/
-mv main.ts app/
+#mv main.ts app/
+# Delete Angular's main.ts; Ionic's is in app/
+rm main.ts
+echo "Copied source files from web"
 
 # Run python script to change source files
 #printf "Modifying source for mobile...\n"
 #../../make_mobile_files.py
 
 cd ..
+echo "Installing dependencies..."
 npm install
+echo "Installed dependencies"
