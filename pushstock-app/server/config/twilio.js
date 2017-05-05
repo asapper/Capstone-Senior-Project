cf3853ab4c7dc5aeaf4efcbac33547b97a92815e
Collin Rapp <collin_rapp@baylor.edu>
2017-05-05 05:54:24 -0500

    Added config/twilio.

---
+++ pushstock-app/server/config/twilio.js
@@ -0,0 +1,7 @@
+var twilioCfg = {}
+
+twilioCfg.accountSid = 'AC99b784b2e936a46605df029ec0322f93';
+twilioCfg.authToken = 'f6c4e61783eb8181ef9a0f9bd77e36b5';
+twilioCfg.sendingNumber = '+19712526887';
+
+module.exports = twilioCfg;
